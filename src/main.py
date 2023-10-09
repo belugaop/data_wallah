8a0437c6328c6eb9d7de6fc0c308562f6ee171ab
BELUGA_XD <96910602+belugaop@users.noreply.github.com>
2023-10-09 16:12:52 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,88 @@
-will be back sooooooooooooooooooooooooooonnnnnnnn very sooooooooooon
+import tkinter as tk
+
+# Function to perform sequence alignment (add your alignment code here)
+def perform_alignment():
+    # Placeholder: Implement sequence alignment logic
+    result_text.delete(1.0, tk.END)  # Clear previous results
+    result_text.insert(tk.END, "Alignment results will be displayed here.")
+
+# Create the main application window
+root = tk.Tk()
+root.title("Sequence Analysis Tool")
+root.geometry("800x600")
+
+# Create a Help button
+def show_help():
+    # Placeholder: Display a help message or user guide
+    pass
+
+help_button = tk.Button(root, text="Help", command=show_help)
+help_button.pack(pady=10)
+
+# Display "Hello [Name]" (replace [Name] with the user's name)
+user_name = "John"  # Replace with the actual user's name
+hello_label = tk.Label(root, text=f"Hello {user_name}", font=("Helvetica", 14))
+hello_label.pack()
+
+# Input fields for sequence entry
+sequence_label = tk.Label(root, text="Enter Sequence:")
+sequence_label.pack()
+sequence_entry = tk.Text(root, height=5, width=50)
+sequence_entry.pack()
+
+# Alignment button
+align_button = tk.Button(root, text="Perform Alignment", command=perform_alignment)
+align_button.pack(pady=10)
+
+# Result display
+result_label = tk.Label(root, text="Alignment Results:")
+result_label.pack()
+result_text = tk.Text(root, height=10, width=50)
+result_text.pack()
+
+# Run the GUI application
+root.mainloop()
+import tkinter as tk
+
+# Function to perform sequence alignment (add your alignment code here)
+def perform_alignment():
+    # Placeholder: Implement sequence alignment logic
+    result_text.delete(1.0, tk.END)  # Clear previous results
+    result_text.insert(tk.END, "Alignment results will be displayed here.")
+
+# Create the main application window
+root = tk.Tk()
+root.title("Sequence Analysis Tool")
+root.geometry("800x600")
+
+# Create a Help button
+def show_help():
+    # Placeholder: Display a help message or user guide
+    pass
+
+help_button = tk.Button(root, text="Help", command=show_help)
+help_button.pack(pady=10)
+
+# Display "Hello [Name]" (replace [Name] with the user's name)
+user_name = "John"  # Replace with the actual user's name
+hello_label = tk.Label(root, text=f"Hello {user_name}", font=("Helvetica", 14))
+hello_label.pack()
+
+# Input fields for sequence entry
+sequence_label = tk.Label(root, text="Enter Sequence:")
+sequence_label.pack()
+sequence_entry = tk.Text(root, height=5, width=50)
+sequence_entry.pack()
+
+# Alignment button
+align_button = tk.Button(root, text="Perform Alignment", command=perform_alignment)
+align_button.pack(pady=10)
+
+# Result display
+result_label = tk.Label(root, text="Alignment Results:")
+result_label.pack()
+result_text = tk.Text(root, height=10, width=50)
+result_text.pack()
+
+# MAIN
+root.mainloop()
