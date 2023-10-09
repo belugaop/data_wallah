463c922c0d92eb184afa263f3d5a70d778e8b424
BELUGA_XD <96910602+belugaop@users.noreply.github.com>
2023-10-09 18:49:30 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,88 +1,42 @@
+# main.py
 import tkinter as tk
+from tkinter import filedialog
+from gui import SequenceAnalyzer
+from sequence_alignment import SequenceAligner
+from database_access import SequenceDatabase
 
-# Function to perform sequence alignment (add your alignment code here)
-def perform_alignment():
-    # Placeholder: Implement sequence alignment logic
-    result_text.delete(1.0, tk.END)  # Clear previous results
-    result_text.insert(tk.END, "Alignment results will be displayed here.")
+class BioinformaticsApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Bioinformatics App")
+        self.root.geometry("800x600")
 
-# Create the main application window
-root = tk.Tk()
-root.title("Sequence Analysis Tool")
-root.geometry("800x600")
+        # Initialize the Sequence Analyzer, Sequence Aligner, and Sequence Database
+        self.sequence_analyzer = SequenceAnalyzer()
+        self.sequence_aligner = SequenceAligner()
+        self.sequence_db = SequenceDatabase("sequence_database.db")
 
-# Create a Help button
-def show_help():
-    # Placeholder: Display a help message or user guide
-    pass
+        # Connect GUI actions to functions
+        self.sequence_analyzer.align_button.config(command=self.perform_alignment)
+        self.sequence_analyzer.load_button.config(command=self.load_sequence_file)
 
-help_button = tk.Button(root, text="Help", command=show_help)
-help_button.pack(pady=10)
+        # Run the GUI
+        self.sequence_analyzer.run()
 
-# Display "Hello [Name]" (replace [Name] with the user's name)
-user_name = "John"  # Replace with the actual user's name
-hello_label = tk.Label(root, text=f"Hello {user_name}", font=("Helvetica", 14))
-hello_label.pack()
+    def perform_alignment(self):
+        sequence = self.sequence_analyzer.sequence_entry.get("1.0", "end-1c")
+        alignment_result = self.sequence_aligner.align_sequences(sequence, "Reference Sequence")
+        self.sequence_analyzer.result_text.delete(1.0, tk.END)
+        self.sequence_analyzer.result_text.insert(tk.END, alignment_result)
 
-# Input fields for sequence entry
-sequence_label = tk.Label(root, text="Enter Sequence:")
-sequence_label.pack()
-sequence_entry = tk.Text(root, height=5, width=50)
-sequence_entry.pack()
+    def load_sequence_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Sequence Files", "*.fasta *.txt")])
+        if file_path:
+            with open(file_path, "r") as file:
+                sequence = file.read()
+                self.sequence_analyzer.sequence_entry.delete("1.0", tk.END)
+                self.sequence_analyzer.sequence_entry.insert(tk.END, sequence)
 
-# Alignment button
-align_button = tk.Button(root, text="Perform Alignment", command=perform_alignment)
-align_button.pack(pady=10)
-
-# Result display
-result_label = tk.Label(root, text="Alignment Results:")
-result_label.pack()
-result_text = tk.Text(root, height=10, width=50)
-result_text.pack()
-
-# Run the GUI application
-root.mainloop()
-import tkinter as tk
-
-# Function to perform sequence alignment (add your alignment code here)
-def perform_alignment():
-    # Placeholder: Implement sequence alignment logic
-    result_text.delete(1.0, tk.END)  # Clear previous results
-    result_text.insert(tk.END, "Alignment results will be displayed here.")
-
-# Create the main application window
-root = tk.Tk()
-root.title("Sequence Analysis Tool")
-root.geometry("800x600")
-
-# Create a Help button
-def show_help():
-    # Placeholder: Display a help message or user guide
-    pass
-
-help_button = tk.Button(root, text="Help", command=show_help)
-help_button.pack(pady=10)
-
-# Display "Hello [Name]" (replace [Name] with the user's name)
-user_name = "John"  # Replace with the actual user's name
-hello_label = tk.Label(root, text=f"Hello {user_name}", font=("Helvetica", 14))
-hello_label.pack()
-
-# Input fields for sequence entry
-sequence_label = tk.Label(root, text="Enter Sequence:")
-sequence_label.pack()
-sequence_entry = tk.Text(root, height=5, width=50)
-sequence_entry.pack()
-
-# Alignment button
-align_button = tk.Button(root, text="Perform Alignment", command=perform_alignment)
-align_button.pack(pady=10)
-
-# Result display
-result_label = tk.Label(root, text="Alignment Results:")
-result_label.pack()
-result_text = tk.Text(root, height=10, width=50)
-result_text.pack()
-
-# MAIN
-root.mainloop()
+if __name__ == "__main__":
+    app = BioinformaticsApp()
+    app.root.mainloop()
