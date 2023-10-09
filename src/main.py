3d84207a3bb910cfb918eb2db00b2f9996cbe8c0
BELUGA_XD <96910602+belugaop@users.noreply.github.com>
2023-10-09 18:54:48 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from gui import SequenceAnalyzer
 from sequence_alignment import SequenceAligner
 from database_access import SequenceDatabase
 from blast_integration import BlastIntegration
+from gc_calculator import GCCalculator
 
 class BioinformaticsApp:
     def __init__(self):
@@ -12,16 +13,18 @@ class BioinformaticsApp:
         self.root.title("Bioinformatics App")
         self.root.geometry("800x600")
 
-        # Initialize the Sequence Analyzer, Sequence Aligner, Sequence Database, and Blast Integration
+        # Initialize the Sequence Analyzer, Sequence Aligner, Sequence Database, Blast Integration, and GC Calculator
         self.sequence_analyzer = SequenceAnalyzer()
         self.sequence_aligner = SequenceAligner()
         self.sequence_db = SequenceDatabase("sequence_database.db")
         self.blast_integration = BlastIntegration()
+        self.gc_calculator = GCCalculator()
 
         # Connect GUI actions to functions
         self.sequence_analyzer.align_button.config(command=self.perform_alignment)
         self.sequence_analyzer.load_button.config(command=self.load_sequence_file)
         self.sequence_analyzer.blast_button.config(command=self.run_blast)
+        self.sequence_analyzer.gc_button.config(command=self.calculate_gc_content)
 
         # Run the GUI
         self.sequence_analyzer.run()
@@ -45,6 +48,12 @@ class BioinformaticsApp:
         blast_result = self.blast_integration.run_blast(sequence)
         self.sequence_analyzer.result_text.delete(1.0, tk.END)
         self.sequence_analyzer.result_text.insert(tk.END, blast_result)
+
+    def calculate_gc_content(self):
+        sequence = self.sequence_analyzer.sequence_entry.get("1.0", "end-1c")
+        gc_content = self.gc_calculator.calculate_gc_content(sequence)
+        self.sequence_analyzer.result_text.delete(1.0, tk.END)
+        self.sequence_analyzer.result_text.insert(tk.END, f"GC Content: {gc_content:.2f}%")
 
 if __name__ == "__main__":
     app = BioinformaticsApp()
